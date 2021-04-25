de8def31de1a04d431f1462152688853a1ed8d63
Sangsawangchai <45919996+Sangsawangchai@users.noreply.github.com>
2021-04-25 14:00:52 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom"
+
+import About from './screens/About'
+import Home from './screens/Home'
+import Users from './screens/Users'
+import Login from './screens/Login'
+
+export default function App() {
+  return (
+    <Router>
+      {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+      <Switch>
+        <Route path="/about" children={About} />
+        <Route path="/users">
+          <Users />
+        </Route>
+        <Route path='/login'>
+          <Login />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+
+
+
